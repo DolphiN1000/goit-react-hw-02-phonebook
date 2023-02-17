33e0720d11e721847dd4367653e7ab6ecca4651d
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-02-17 21:01:11 +0200

    Update ContactList.jsx

--- src/components/Phonebook/ContactList/ContactList.jsx
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -1,17 +1,31 @@
+import PropTypes from 'prop-types';
+
 import styles from './contactList.module.scss';
 
-const ContactsList = ({items, deleteContack}) => {
- const contacts = items.map(({id, name, number, deleteContack}) => <li key={id}>{name}: {number}<button id={id} onClick={() => deleteContack(id)}>Delete</button></li> );
+const ContactsList = ({ items, deleteContack }) => {
+  const contacts = items.map(({ id, name, number, deleteContack }) => (
+    <li key={id}>
+      {name}: {number}
+      <button id={id} onClick={() => deleteContack(id)}>
+        Delete
+      </button>
+    </li>
+  ));
 
- return (
-    <ol>
-        {contacts}
-    </ol>
- )
-}
+  return <ol>{contacts}</ol>;
+};
 
 export default ContactsList;
 
 ContactsList.defaultProps = {
-    items: [],
+  items: [],
+};
+
+ContactsList.propTypes = {
+    items: PropTypes.shape({
+id: PropTypes.string.isRequired,
+name: PropTypes.string.isRequired,
+number:PropTypes.string.isRequired}),
+deleteContack: PropTypes.func.isRequired,
+
 }
