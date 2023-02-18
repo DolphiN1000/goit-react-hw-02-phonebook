f563ecbabd19a1fe41b5ad808f3c5a7417351639
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-02-18 22:47:24 +0200

    13

---
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import PropTypes from 'prop-types';
+
+import styles from './contactForm.module.scss';
+
+class ContactsForm extends Component {
+    state = {}
+}
