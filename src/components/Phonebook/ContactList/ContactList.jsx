b5146454aa418ba63e6e2bfdf1bd5db1dd901b8c
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-02-18 14:26:59 +0200

    fix delete

--- src/components/Phonebook/ContactList/ContactList.jsx
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -2,30 +2,32 @@ import PropTypes from 'prop-types';
 
 import styles from './contactList.module.scss';
 
-const ContactsList = ({ items, deleteContack }) => {
-  const contacts = items.map(({ id, name, number, deleteContack }) => (
+const ContactsList = ({ contacts, deleteContact }) => {
+  const contactsList = contacts.map(({ id, name, number}) => (
     <li key={id}>
       {name}: {number}
-      <button id={id} onClick={() => deleteContack(id)}>
+      <button id={id} onClick={() => deleteContact(id)}>
         Delete
       </button>
     </li>
   ));
 
-  return <ol>{contacts}</ol>;
+  return <ol>{contactsList}</ol>;
 };
 
 export default ContactsList;
 
 ContactsList.defaultProps = {
-  items: [],
+  contacts: [],
 };
 
 ContactsList.propTypes = {
-    items: PropTypes.shape({
-id: PropTypes.string.isRequired,
-name: PropTypes.string.isRequired,
-number:PropTypes.string.isRequired}),
-deleteContack: PropTypes.func.isRequired,
-
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  deleteContact: PropTypes.func.isRequired
 }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,47 +1,51 @@
-import { Component } from "react";
-import { nanoid } from "nanoid";
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
-import ContactsList from "./ContactList/ContactList";
+import ContactsList from './ContactList/ContactList';
 
 class Phonebook extends Component {
-    state = {
-        contacts: [
-          {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-          {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-          {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-          {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-        ],
-        filter: '',
-        name: '',
-        number: ''
-      }
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
 
-      deleteContack(id){
-        this.setState(({items}) => {
-          const newContacts = items.filter(item => item.id !== id);
-          return {items:newContacts}
-        })
+  deleteContact = id => {
+    this.setState(({ contacts }) => {
+      const newContacts = contacts.filter(contact => contact.id !== id);
+      return { contacts: newContacts };
+    });
+  };
 
-      }
-
-      render() {return(
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <p>Name</p>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <button>Add contact</button>
         <div>
-            <h1>Phonebook</h1>
-            <p>Name</p>
-            <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-/>
-<button>Add contact</button>
-<div>
-    <h2>Contacts</h2>
-  <ContactsList items={this.state.contacts} deleteContack={this.deleteContack}/>
-</div>
+          <h2>Contacts</h2>
+          <ContactsList
+            contacts={this.state.contacts}
+            deleteContact={this.deleteContact}
+          />
         </div>
-      )}
+      </div>
+    );
+  }
 }
 
 export default Phonebook;
